8f46bce5ded9769b69ec987cfda44f22a15c7851
ADA-BAMBOO <ADA-BAMBOO@users.noreply.github.com>
2026-08-01 02:13:18 +0000

    Supabase backup

--- prisma/backups/data.sql
+++ prisma/backups/data.sql
@@ -4,7 +4,7 @@ SET session_replication_role = replica;
 -- PostgreSQL database dump
 --
 
--- \restrict nhThYcHPfwnlVm4d6laRn7NnukAGe7wtOyZvAHoeHgIZu9VbecDlIYFuU3hV7sH
+-- \restrict EFERH8lH9HVuELTvCiZNUS91KNHpiLxPqTE81o9YjhaHsxa61ZF7zwEnoAJDAWq
 
 -- Dumped from database version 17.6
 -- Dumped by pg_dump version 17.6
@@ -1193,6 +1193,6 @@ SELECT pg_catalog.setval('"supabase_functions"."hooks_id_seq"', 1, false);
 -- PostgreSQL database dump complete
 --
 
--- \unrestrict nhThYcHPfwnlVm4d6laRn7NnukAGe7wtOyZvAHoeHgIZu9VbecDlIYFuU3hV7sH
+-- \unrestrict EFERH8lH9HVuELTvCiZNUS91KNHpiLxPqTE81o9YjhaHsxa61ZF7zwEnoAJDAWq
 
 RESET ALL;
